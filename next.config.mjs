7e4df36412e0042b01efdcc798f06c91f344210b
SAAbir <shahalamabir21@gmail.com>
2024-06-28 17:43:03 +0600

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    basePath: '/CampTraveller',
+    assetPrefix: '/CampTraveller/',
+  };
+  
+  export default nextConfig;
